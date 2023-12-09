eabc08bc06f37727a6ee03cb4b88267b7d2ab8c6
Orion Kindel <orionkindel@gmail.com>
2023-12-08 20:36:55 -0600

    fix: catch execution context errors, pedantic packages

--- src/Puppeteer.FFI.purs
+++ src/Puppeteer.FFI.purs
@@ -2,22 +2,27 @@ module Puppeteer.FFI (mapToRecord, maybeToUndefined, mergeRecords, unsafeMaybeTo
 
 import Prelude
 
+import Control.Monad.Error.Class (catchError, throwError)
 import Control.Monad.Except (runExcept)
 import Control.Promise (Promise)
 import Control.Promise as Promise
 import Data.Array as Array
 import Data.Either (either)
+import Data.Foldable (any)
 import Data.FoldableWithIndex (foldlWithIndex)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe)
 import Data.Nullable (Nullable)
 import Data.Nullable as Nullable
+import Data.String as String
+import Data.String.Utils (includes) as String
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Exception (error)
+import Effect.Exception as Error
 import Foreign (Foreign, unsafeReadTagged)
 import Simple.JSON (class WriteForeign)
 
@@ -39,10 +44,30 @@ unsafeMaybeToUndefined :: forall a. Maybe a -> Foreign
 unsafeMaybeToUndefined = _maybeToUndefined Nullable.toNullable
 
 promiseToAff :: forall a. Effect (Promise a) -> Aff a
-promiseToAff =
+promiseToAff work =
   let
     err e = either (const $ error $ anyToString e) identity
       $ runExcept
       $ unsafeReadTagged "Error" e
+
+    retryErrorsMatching =
+      [ "execution context destroyed"
+      ]
+
+    shouldRetry e =
+      any
+        (\retryErr -> String.includes retryErr $ String.trim (Error.message e))
+        retryErrorsMatching
+
+    attempt = do
+      promise <- liftEffect work
+      Promise.toAff' err promise
+
+    retry e =
+      if shouldRetry e then do
+        promise <- liftEffect work
+        Promise.toAff' err promise
+      else
+        throwError e
   in
-    flip bind (Promise.toAff' err) <<< liftEffect
+    catchError attempt retry
